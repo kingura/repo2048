f1ce92b20ee64b6b058717692ecf61be9dec4d93
kingura <kopets@i.ua>
2015-12-16 01:23:05 +0200

    added sln

---
+++ 2048_WF/Field.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048_WF
+{
+    class Field
+    {
+        // константы
+        public const int default_dimension = 4;
+
+
+        // свойства
+        public int Dimension { get; private set; }
+        public int[,] arrayGrid;
+
+        // конструкторы
+        public Field(int dim)
+        {
+            Dimension = dim;
+            arrayGrid = new int[Dimension, Dimension];
+            // обнуляем массив
+            for (int i = 0; i < arrayGrid.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayGrid.GetLength(1); j++)
+                {
+                    arrayGrid[i, j] = 0;
+                }
+            }
+
+            // нарисовать
+            //Draw(MainForm);
+
+        }
+        public Field() : this(default_dimension) { }
+
+
+        // методы
+        public void Draw(System.Windows.Forms.Form form)
+        {
+            System.Drawing.Graphics formGraphics = form.CreateGraphics();
+            System.Drawing.Pen myPen = new System.Drawing.Pen(System.Drawing.Color.Black);
+            formGraphics.DrawRectangle(myPen, new System.Drawing.Rectangle(50, 50, 200, 200));
+
+            // очищаем экземпляры
+            myPen.Dispose();
+            formGraphics.Dispose();
+        }
+    }
+}
---
+++ 2048_WF/Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _2048_WF
+{
+
+    class Number
+    {
+        public const int Width = 50;
+        public const int Height = 50;
+        public const byte Multiplier = 2;
+
+        public int Value { get; private set; }
+
+        // конструктор
+        public Number()
+        {
+            Random rand = new Random();
+            Value = rand.Next(1, 3) * Multiplier;
+        }
+    }
+}
