60c6a9fa7b89d14cd6a3136de727ed5b931dfc12
kingura <kopets@i.ua>
2016-01-25 05:37:18 +0200

    grid_draw_25.01

--- 2048_WF/Number.cs
+++ 2048_WF/Number.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 
 namespace _2048_WF
@@ -10,17 +10,27 @@ namespace _2048_WF
 
     class Number
     {
-        public const int Width = 50;
-        public const int Height = 50;
-        public const byte Multiplier = 2;
-
         public int Value { get; private set; }
 
-        // конструктор
+        // конструктор остается "по умолчанию"
         public Number()
         {
+            Value = 0;
+        }
+
+        // функция рандомного числа
+        public void NewRandNum(byte multiplier)
+        {
+            //Правила:
+            //После каждого хода на случайной пустой клетке появляется новая плитка
+            //номинала «2» (с вероятностью 90%) или «4» (с вероятностью 10%).
             Random rand = new Random();
-            Value = rand.Next(1, 3) * Multiplier;
+            int randVal = rand.Next(1, 101);
+            Value = randVal * multiplier;
+            if (randVal >= 91)
+            {
+                Value = Value*2;
+            }
         }
     }
 }
